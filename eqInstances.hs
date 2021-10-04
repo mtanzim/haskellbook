59717a400ca627b72d226bc83e95ea16915ebb04
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-10-04 18:24:17 -0400

    eq instances exercise complete

--- eqInstances.hs
+++ eqInstances.hs
@@ -26,7 +26,25 @@ data Pair a = Pair a a
 instance Eq a => Eq (Pair a) where
   (==) (Pair x y) (Pair x' y') = x == x' && y == y'
 
-data Tuple a b = Tuple a Benchmark
+data Tuple a b = Tuple a b
 
 instance (Eq a, Eq b) => Eq (Tuple a b) where
-    (==) (Tuple x y) (Tuple x' y') = x == x' && y == y'
+  (==) (Tuple x y) (Tuple x' y') = x == x' && y == y'
+
+data Which a
+  = ThisOne a
+  | ThatOne a
+
+instance Eq a => Eq (Which a) where
+  (==) (ThisOne a) (ThisOne a') = a == a'
+  (==) (ThatOne a) (ThatOne a') = a == a'
+  (==) _ _ = False
+
+data EitherOr a b
+  = Hello a
+  | Goodbye b
+
+instance (Eq a, Eq b) => Eq (EitherOr a b) where
+  (==) (Hello a) (Hello a') = a == a'
+  (==) (Goodbye a) (Goodbye a') = a == a'
+  (==) _ _ = False
