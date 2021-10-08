3845fa7b426b4e6a2194b56e56d722e06ffb7c09
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-10-08 13:11:53 -0400

    type class type kwon do

--- typeClasses.hs
+++ typeClasses.hs
@@ -85,3 +85,9 @@ mySort = sort
 
 signifier :: Ord a => [a] -> a
 signifier xs = head (mySort xs)
+
+chk :: Eq b => (a -> b) -> a -> b -> Bool
+chk aToB a b = aToB a == b
+
+arith :: Num b => (a -> b) -> Integer -> a -> b
+arith aToB i = aToB
