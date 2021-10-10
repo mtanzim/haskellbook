5be69bb5cb6f4433c149e0c04e5d3cbf22d64d96
Tanzim Mokammel <mtanzim@gmail.com>
2021-10-09 22:04:18 -0300

    type guards

--- ch07_funcy.hs
+++ ch07_funcy.hs
@@ -47,3 +47,20 @@ oneIsOne = dodgy 1
 
 oneIsTwo :: Integer -> Integer
 oneIsTwo = (flip dodgy) 2
+
+-- Guards
+avgGrade :: (Ord a, Fractional a) => a -> Char
+avgGrade x
+  | y >= 0.9 = 'A'
+  | y >= 0.8 = 'B'
+  | y >= 0.7 = 'C'
+  | y >= 0.59 = 'D'
+  | y < 0.59 = 'F'
+  where
+    y = x / 100
+
+numbers :: (Ord a, Num a, Num p) => a -> p
+numbers x
+  | x < 0 = -1
+  | x == 0 = 0
+  | x > 0 = 1
