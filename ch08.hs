104089cbd285afbfd3f01bf96e36d724931e528f
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-10-15 08:49:33 -0400

    divide by zero

--- ch08.hs
+++ ch08.hs
@@ -4,3 +4,33 @@ module Ch08 where
 applyTimes :: (Eq a, Num a) => a -> (b -> b) -> b -> b
 applyTimes 0 f b = b
 applyTimes n f b = f (applyTimes (n - 1) f b)
+
+-- exercises
+sumFrom :: (Eq t, Num t) => t -> t
+sumFrom n
+  | n == 1 = n
+  | otherwise = n + sumFrom (n - 1)
+
+mult :: Integral a => a -> a -> a
+mult a b
+  | b == 1 = a
+  | otherwise = a + mult a (b - 1)
+
+data DividedResult  = 
+    Result Integer | DividedByZero
+    deriving Show
+
+dividedBy :: Integral a => a -> a -> DividedResult
+dividedBy num denom = 
+    case denom of
+        0 -> DividedByZero
+        _ -> Result (go (abs num) (abs denom) 0)
+            where
+                go n d count
+                    | n < d = count * modifier
+                    | otherwise = go (n - d) d (count + 1)
+                        where modifier =
+                                case (num < 0, denom < 0) of
+                                (True, True) -> 1
+                                (False, False) -> 1
+                                _ -> (-1)
