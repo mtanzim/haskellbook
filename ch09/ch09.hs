49079f54329741e580f52acf48c760e0a6f5f3c6
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-10-21 14:40:26 -0400

    cons exercises

---
+++ ch09/ch09.hs
@@ -0,0 +1,40 @@
+module Ch09 where
+
+import Text.Parsec (endBy)
+
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (a : _) = Just a
+
+-- TODO: better way to do this?
+eftBool :: Bool -> Bool -> [Bool]
+eftBool start end =
+  case (start, end) of
+    (True, False) -> []
+    (False, True) -> [False, True]
+    (False, False) -> [False]
+    (True, True) -> [True]
+
+
+-- TODO: idiomatic to reverse at the end?
+-- TODO: How to share this exact code between types?
+eftOrdering :: Ordering -> Ordering -> [Ordering]
+eftOrdering start end = go start end []
+  where
+    go start' end' lst
+      | start' > end' = reverse lst
+      | otherwise = go (succ start') end' (start' : lst)
+
+eftInt :: Int -> Int -> [Int]
+eftInt start end = go start end []
+  where
+    go start' end' lst
+      | start' > end' = reverse lst
+      | otherwise = go (succ start') end' (start' : lst)
+
+eftChar :: Char -> Char -> [Char]
+eftChar start end = go start end []
+  where
+    go start' end' lst
+      | start' > end' = reverse lst
+      | otherwise = go (succ start') end' (start' : lst)
--- ch09/safeHead.hs
+++
@@ -1,6 +0,0 @@
-module SafeHead where
-
-safeHead :: [a] -> Maybe a
-safeHead [] = Nothing 
-safeHead (a:_) = Just a
-
