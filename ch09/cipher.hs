3911a4c38be7624cc9e1472acb766534e165fffd
Tanzim Mokammel <mtanzim@gmail.com>
2021-10-22 16:09:24 -0400

    factor out common function

--- ch09/cipher.hs
+++ ch09/cipher.hs
@@ -2,15 +2,17 @@ module Cipher where
 
 import Data.Char (chr, ord)
 
-caesar :: Int -> [Char] -> [Char]
-caesar n = map (chr . fn)
+common :: (Int -> Int -> Int) -> Int -> [Char] -> [Char]
+common op n = map (chr . fn)
   where
     fn x = (+) 97 (mod ((-) (sumVal x) 97) 26)
-    sumVal x = (+) (ord x) (mod n 26) 
+    sumVal x = op (ord x) (mod n 26)
 
+caesar :: Int -> [Char] -> [Char]
+caesar = common (+)
 
 unCaesar :: Int -> [Char] -> [Char]
-unCaesar n = map (chr . fn)
-  where
-    fn x = (+) 97 (mod ((-) (sumVal x) 97) 26)
-    sumVal x = (-) (ord x) (mod n 26)
+unCaesar = common (-)
+
+isValid :: Bool
+isValid = unCaesar 1 (caesar 1 "abracadbra") == "abracadbra"
