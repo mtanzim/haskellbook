f359fa42b86d11511ecbfa7ff55e0abca64dab99
Tanzim Mokammel <mtanzim@gmail.com>
2021-10-22 13:35:36 -0400

    filters

--- ch09/ch09.hs
+++ ch09/ch09.hs
@@ -35,3 +35,30 @@ eftChar start end = go start end []
     go start' end' lst
       | start' > end' = reverse lst
       | otherwise = go (succ start') end' (start' : lst)
+
+-- comprehend thy list
+
+mySqr :: [Integer]
+mySqr = [x ^ 2 | x <- [1 .. 5]]
+
+myCube = [x ^ 3 | x <- [1 .. 5]]
+
+mySquareCubes :: Int -> [(Integer, Integer)]
+mySquareCubes = flip take [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+-- filtering
+multOf3 :: [Integer] -> [Integer]
+multOf3 = filter (\x -> rem x 3 == 0)
+
+multOf3Len :: [Integer] -> Int
+multOf3Len = length . (filter (\x -> rem x 3 == 0))
+
+
+myFilter :: String -> [String]
+myFilter = filterFn . words where
+  filterFn = filter f where
+  f w 
+      | w == "the" = False
+      | w == "a" = False
+      | w == "an" = False
+      | otherwise = True
