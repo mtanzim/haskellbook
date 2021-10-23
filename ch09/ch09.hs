f5f930a511544ef275f2051605279927810518a4
Tanzim Mokammel <mtanzim@gmail.com>
2021-10-23 15:40:15 -0400

    start std list functions

--- ch09/ch09.hs
+++ ch09/ch09.hs
@@ -109,3 +109,12 @@ capAll (x:xs) = toUpper x : capAll xs
 
 getFirstCap :: [Char] -> Char
 getFirstCap = head . capFirst
+
+myOr :: [Bool] -> Bool 
+myOr [] = False
+myOr (x:xs) = x || myOr xs
+
+myAny :: (a -> Bool) -> [a] -> Bool
+myAny f [] = False
+myAny f (x:xs) = f x || myAny f xs
+
