c9e5884746a0fd70173b8094475f3ef9831e45ff
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-10-26 18:20:20 -0400

    chapter review

--- ch10/ch10.hs
+++ ch10/ch10.hs
@@ -1,7 +1,43 @@
 module Ch10 where
 
+fibs :: [Integer]
 fibs = 1 : scanl (+) 1 fibs
 
+fibsN :: Int -> Integer
 fibsN x = fibs !! x
 
+fibsCapped :: Int -> [Integer]
 fibsCapped = (flip take) fibs
+
+fibsFiltered :: [Integer]
+fibsFiltered = takeWhile (< 100) fibs
+
+factorial :: [Integer]
+factorial = scanl (*) 1 [2 ..]
+
+-- chapter exercise
+
+stops :: [Char]
+stops = "pbtdkg"
+
+vowels :: [Char]
+vowels = "aeiou"
+
+combineStopsVowels :: [(Char, Char, Char)]
+combineStopsVowels = [(s, v, s') | s <- stops, v <- vowels, s' <- stops]
+
+combineStopsVowelsWithFilter :: [(Char, Char, Char)]
+combineStopsVowelsWithFilter = [(s, v, s') | s <- stops, s == 'p', v <- vowels, s' <- stops]
+
+nouns = ["Sarah", "Tanzim", "John", "Allan"]
+
+verbs = ["treats", "hates", "misses", "disses"]
+
+combineNounsVerbs :: [([Char], [Char], [Char])]
+combineNounsVerbs = [(s, v, s') | s <- nouns, v <- verbs, s' <- nouns]
+
+seekritFunc :: String -> Double
+seekritFunc x = totalChars / totalWords
+  where
+    totalChars = fromIntegral (sum (map length (words x)))
+    totalWords = fromIntegral (length (words x))
