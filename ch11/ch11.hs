e705ce9667c6526406dd96687999c239897c14e3
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-01 16:40:18 -0400

    all programmers

--- ch11/ch11.hs
+++ ch11/ch11.hs
@@ -76,3 +76,39 @@ data Lilac = Lilac deriving (Show)
 
 -- Normal form
 data Garden = GardenisGarden Gardener Gardenis | DaisyGarden Gardener Daisy | RoseGarden Gardener Rose | LilacGarden Gardener Lilac deriving (Show)
+
+-- programmers
+data OperatingSystem
+  = GnuPlusLinux
+  | OpenBSDPlusNevermindJustBSDStill
+  | Mac
+  | Windows
+  deriving (Eq, Show)
+
+data ProgLang
+  = Haskell
+  | Agda
+  | Idris
+  | PureScript
+  deriving (Eq, Show)
+
+data Programmer = Programmer
+  { os :: OperatingSystem,
+    lang :: ProgLang
+  }
+  deriving (Eq, Show)
+
+allOperatingSystems :: [OperatingSystem]
+allOperatingSystems =
+  [ GnuPlusLinux,
+    OpenBSDPlusNevermindJustBSDStill,
+    Mac,
+    Windows
+  ]
+
+allLanguages :: [ProgLang]
+allLanguages =
+  [Haskell, Agda, Idris, PureScript]
+
+allProgrammgers :: [Programmer]
+allProgrammgers = [Programmer {os = os', lang = lang'} | os' <- allOperatingSystems, lang' <- allLanguages]
