30ea320b4f4d48e29c8b26ee68b5bd6e3540b86f
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-02 22:49:41 -0400

    refactor

--- ch11/chExercises.hs
+++ ch11/chExercises.hs
@@ -2,17 +2,14 @@ module Ch11Exercises where
 
 import Data.Char (chr, ord)
 
-shiftVal :: Char -> Int
-shiftVal c = ord c - 97
-
-common :: (Int -> Int -> Int) -> Int -> Char -> Char
-common op n = chr . fn
-  where
-    fn x = (+) 97 (mod ((-) (sumVal x) 97) 26)
-    sumVal x = op (ord x) (mod n 26)
-
+-- TODO: name these functions better
 caesar :: Int -> Char -> Char
-caesar = common (+)
+caesar = fn (+)
+  where
+    fn op n = chr . fn'
+      where
+        fn' x = (+) 97 (mod ((-) (sumVal x) 97) 26)
+        sumVal x = op (ord x) (mod n 26)
 
 -- TODO: spaces do not work
 cipherVignere :: String -> String -> String
@@ -21,9 +18,11 @@ cipherVignere xs ks =
   where
     go xs' i =
       case xs' of
-        (head : []) -> (caesar (shiftVal curChar) head) : []
-        (head : tail) -> (caesar (shiftVal curChar) head) : go tail (i + 1)
+        [head] -> [caesar curShiftVal head]
+        (head : tail) -> caesar curShiftVal head : go tail (i + 1)
         _ -> []
       where
+        curShiftVal = shiftVal curChar
         curChar = ks !! curI
         curI = mod i (length ks)
+        shiftVal c = ord c - 97
