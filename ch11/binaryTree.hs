88aba8cd07ef6239ebbf4f8de6b9e53b8b008d10
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-02 22:03:47 -0400

    tree traversals

--- ch11/binaryTree.hs
+++ ch11/binaryTree.hs
@@ -28,3 +28,39 @@ mapOkay =
   if mapTree (+ 1) testTree' == mapExpected
     then print "yup OK!"
     else error "test failed!"
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ (preorder left) ++ (preorder right)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = (postorder left) ++ (postorder right) ++ [a]
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = (inorder left) ++ [a] ++ (inorder right)
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3]
+    then putStrLn "Preorder fine!"
+    else putStrLn "Bad news bears."
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3]
+    then putStrLn "Inorder fine!"
+    else putStrLn "Bad news bears."
+
+testPostOrder :: IO ()
+testPostOrder =
+  if postorder testTree == [1, 3, 2]
+    then putStrLn "Postorder fine!"
+    else putStrLn "Bad news bears."
+
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree f z t = foldr f z (inorder t)
