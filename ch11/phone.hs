993ccbd65581d2b91f393d32a5508f476b16f70f
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-04 17:19:12 -0400

    getting somewhere

--- ch11/phone.hs
+++ ch11/phone.hs
@@ -1,5 +1,7 @@
 module Phone where
 
+import Data.Char
+
 type Digit = Char
 
 type Presses = Int
@@ -13,12 +15,12 @@ data PhoneButton = PhoneButton Digit PossibleChars
 type DaPhone = [PhoneButton]
 
 phone :: DaPhone
-phone = [PhoneButton '2' "abc2", PhoneButton '3' "def3", PhoneButton '4' "ghi4", PhoneButton '5' "jkl5", PhoneButton '6' "mno6"]
+phone = [PhoneButton '*' "", PhoneButton '#' ".,", PhoneButton '0' " 0", PhoneButton '1' "1", PhoneButton '2' "abc2", PhoneButton '3' "def3", PhoneButton '4' "ghi4", PhoneButton '5' "jkl5", PhoneButton '6' "mno6", PhoneButton '7' "pqrs", PhoneButton '8' "TUV", PhoneButton '9' "wxyz"]
 
 reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
-reverseTaps p c = [digit p]
+reverseTaps p c = if isUpper c then ('*', 1) : [digit p (toLower c)] else [digit p c]
   where
-    digit ((PhoneButton d chars) : tail) = if elem c chars then (d, getPos chars 0) else digit tail
-    digit [] = ('!', -1)
-    getPos (curChar : rest) curIdx = if curChar == c then curIdx + 1 else getPos rest curIdx + 1
-    getPos [] _ = -1
+    digit ((PhoneButton d chars) : tail) c' = if elem c' chars then (d, getPos chars 0 c') else digit tail c'
+    digit [] _ = ('!', -1)
+    getPos (curChar : rest) curIdx c'' = if curChar == c'' then curIdx + 1 else getPos rest (curIdx + 1) c''
+    getPos [] _ _ = -2
