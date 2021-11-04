85ae83afb7cb7c7395183052823a4e1e05334470
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-04 18:25:18 -0400

    most popular

--- ch11/phone.hs
+++ ch11/phone.hs
@@ -1,5 +1,6 @@
 module Phone where
 
+import Control.Arrow (Arrow (second))
 import Data.Char
 import System.Posix.Terminal.ByteString (sendBreak)
 
@@ -15,6 +16,7 @@ data PhoneButton = PhoneButton Digit PossibleChars
 
 type DaPhone = [PhoneButton]
 
+-- TODO: this should be a hash map or similar
 phone :: DaPhone
 phone = [PhoneButton '*' "", PhoneButton '#' ".,", PhoneButton '0' " 0", PhoneButton '1' "1", PhoneButton '2' "abc2", PhoneButton '3' "def3", PhoneButton '4' "ghi4", PhoneButton '5' "jkl5", PhoneButton '6' "mno6", PhoneButton '7' "pqrs", PhoneButton '8' "tuv", PhoneButton '9' "wxyz"]
 
@@ -31,3 +33,15 @@ cellPhonesDead p sentence = foldr ((++)) [] (map (\c -> reverseTaps p c) sentenc
 
 fingerTaps :: [(Digit, Presses)] -> Presses
 fingerTaps = (foldr ((+)) 0 . map (\(d, p) -> p))
+
+counts :: Eq a => a -> [a] -> Int
+counts c = length . (filter (\x -> x == c))
+
+countsList :: String -> [(Char, Int)]
+countsList s = map (\c -> (c, counts c s)) s
+
+countsMax :: [(Char, Int)] -> (Char, Int)
+countsMax xs = foldr (\acc cur -> if (snd cur) > (snd acc) then cur else acc) (head xs) xs
+
+mostPopularLetter :: String -> Char
+mostPopularLetter = (fst . countsMax . countsList)
