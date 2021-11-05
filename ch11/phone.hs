d500591dfe2aa3727f706def091853ca53990396
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-04 20:57:11 -0400

    t9 exercise completed

--- ch11/phone.hs
+++ ch11/phone.hs
@@ -1,16 +1,12 @@
 module Phone where
 
-import Control.Arrow (Arrow (second))
-import Data.Char
-import System.Posix.Terminal.ByteString (sendBreak)
+import Data.Char (isUpper, toLower)
 
 type Digit = Char
 
 type Presses = Int
 
 type PossibleChars = String
-
--- data T9Press = T9press Button
 
 data PhoneButton = PhoneButton Digit PossibleChars
 
@@ -19,6 +15,19 @@ type DaPhone = [PhoneButton]
 -- TODO: this should be a hash map or similar
 phone :: DaPhone
 phone = [PhoneButton '*' "", PhoneButton '#' ".,", PhoneButton '0' " 0", PhoneButton '1' "1", PhoneButton '2' "abc2", PhoneButton '3' "def3", PhoneButton '4' "ghi4", PhoneButton '5' "jkl5", PhoneButton '6' "mno6", PhoneButton '7' "pqrs", PhoneButton '8' "tuv", PhoneButton '9' "wxyz"]
+
+convo :: [String]
+convo =
+  [ "Wanna play 20 questions",
+    "Ya",
+    "U 1st haha",
+    "Lol OK. Have u ever tasted alcohol",
+    "Lol ya",
+    "Wow ur cool haha. Ur turn",
+    "OK. Do u think I am pretty Lol",
+    "Lol ya",
+    "Just making sure rofl ur turn"
+  ]
 
 reverseTaps :: DaPhone -> Char -> [(Digit, Presses)]
 reverseTaps p c = if isUpper c then ('*', 1) : [digit p (toLower c)] else [digit p c]
@@ -37,11 +46,21 @@ fingerTaps = (foldr ((+)) 0 . map (\(d, p) -> p))
 counts :: Eq a => a -> [a] -> Int
 counts c = length . (filter (\x -> x == c))
 
-countsList :: String -> [(Char, Int)]
+countsList :: Eq a => [a] -> [(a, Int)]
 countsList s = map (\c -> (c, counts c s)) s
 
-countsMax :: [(Char, Int)] -> (Char, Int)
-countsMax xs = foldr (\acc cur -> if (snd cur) > (snd acc) then cur else acc) (head xs) xs
+countsMax :: Eq a => [(a, Int)] -> (a, Int)
+countsMax xs = foldr (\acc cur -> if snd cur > snd acc then cur else acc) (head xs) xs
 
+-- TODO: yikes what is the time complexity here?
 mostPopularLetter :: String -> Char
-mostPopularLetter = (fst . countsMax . countsList)
+mostPopularLetter = fst . countsMax . countsList
+
+costOfMostPopularLetter :: String -> Presses
+costOfMostPopularLetter = fingerTaps . reverseTaps phone . mostPopularLetter
+
+coolestLtr :: [String] -> Char
+coolestLtr = mostPopularLetter . concat
+
+coolestWord :: [String] -> String
+coolestWord = fst . countsMax . countsList . words . foldr (\acc x -> x ++ " " ++ acc) ""
