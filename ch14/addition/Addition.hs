b0a45ce1741ac06c3a27539157f7a1be233d04a3
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-15 16:30:30 -0500

    .

--- ch14/addition/Addition.hs
+++ ch14/addition/Addition.hs
@@ -1,5 +1,24 @@
 module Addition where
 
+import Test.Hspec
+
 sayHello :: IO ()
 sayHello = do
   putStrLn "hello world"
+
+dividedBy :: Integral a => a -> a -> (a, a)
+dividedBy num denom = go num denom 0
+  where
+    go n d count
+      | n < d = (count, n)
+      | otherwise = go (n - d) d (count + 1)
+
+main :: IO ()
+main = hspec $ do
+  describe "addition" $ do
+    it "1 + 1 is greater than 1" $ do
+      (1 + 1) > 1 `shouldBe` True
+    it "2 + 2 is equal to 4" $ do
+      (2 + 2) `shouldBe` 4
+    it "22 divided by 5 is 4 remainder 2" $ do
+      dividedBy 22 5 `shouldBe` (4, 2)
