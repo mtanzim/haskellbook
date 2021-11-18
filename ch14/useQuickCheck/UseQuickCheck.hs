ff61580e4a9e2ea579ce92304b414496baa4441e
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-18 14:01:46 -0500

    more quickcheck tests

--- ch14/useQuickCheck/UseQuickCheck.hs
+++ ch14/useQuickCheck/UseQuickCheck.hs
@@ -10,14 +10,64 @@ half' = (* 2) . half
 plusAssociative :: Int -> Int -> Int -> Bool
 plusAssociative x y z = x + (y + z) == (x + y) + z
 
-plusCommutative :: Int -> Int -> Int -> Bool
-plusCommutative x y z = x + (y + z) == (x + y) + z
+plusCommutative :: Int -> Int -> Bool
+plusCommutative x y = x + y == y + x
+
+multAssociative :: Int -> Int -> Int -> Bool
+multAssociative x y z = x * (y * z) == (x * y) * z
+
+multCommutative :: Int -> Int -> Bool
+multCommutative x y = x * y == y * x
 
 runQcPlusAssociative :: IO ()
 runQcPlusAssociative = quickCheck plusAssociative
 
 runQcPlusCommutative :: IO ()
 runQcPlusCommutative = quickCheck plusCommutative
+
+runQcMultAssociative :: IO ()
+runQcMultAssociative = quickCheck multAssociative
+
+runQcMultCommutative :: IO ()
+runQcMultCommutative = quickCheck multCommutative
+
+quotRemProp :: Gen Bool
+quotRemProp = do
+  x <- choose (1, 9999) :: Gen Integer
+  y <- choose (1, 9999) :: Gen Integer
+  return $ (quot x y) * y + (rem x y) == x
+
+divModProp :: Gen Bool
+divModProp = do
+  x <- choose (1, 9999) :: Gen Integer
+  y <- choose (1, 9999) :: Gen Integer
+  return $ (div x y) * y + (mod x y) == x
+
+runQuotRemProp = quickCheck quotRemProp
+
+runDivModProp = quickCheck divModProp
+
+expCommutative :: Int -> Int -> Int -> Bool
+expCommutative x y z = x ^ (y ^ z) == (x ^ y) ^ z
+
+expAssociative :: Int -> Int -> Bool
+expAssociative x y = x ^ y == y ^ x
+
+runExpCommutative :: IO ()
+runExpCommutative = quickCheck expCommutative
+
+runExpAssociative :: IO ()
+runExpAssociative = quickCheck expAssociative
+
+reverseProp :: String -> Bool
+reverseProp s = (reverse . reverse) s == id s
+
+runReverseProp = quickCheck reverseProp
+
+fnProp :: (String -> Int) -> String -> Bool
+fnProp f s = (f s) == (f $ s)
+
+runFnProp = quickCheck (fnProp (\s -> length s))
 
 main :: IO ()
 main = hspec $ do
