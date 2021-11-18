17ca26534ec336eb4db01c546092519271787214
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-17 21:03:37 -0500

    more arbitrary

--- ch14/addition/Addition.hs
+++ ch14/addition/Addition.hs
@@ -3,7 +3,6 @@ module Addition where
 import Data.Int (Int16, Int32, Int8)
 import Test.Hspec
 import Test.QuickCheck
-import Test.QuickCheck (Arbitrary)
 
 sayHello :: IO ()
 sayHello = do
@@ -55,6 +54,43 @@ genMaybe :: Arbitrary a => Gen (Maybe a)
 genMaybe = do
   a <- arbitrary
   elements [Nothing, Just a]
+
+data Trivial = Trivial deriving (Eq, Show)
+
+trivialGen :: Gen Trivial
+trivialGen = return Trivial
+
+instance Arbitrary Trivial where
+  arbitrary = trivialGen
+
+data Identity a = Identity a deriving (Eq, Show)
+
+identityGen :: Arbitrary a => Gen (Identity a)
+identityGen = do
+  a <- arbitrary
+  return (Identity a)
+
+instance Arbitrary a => Arbitrary (Identity a) where
+  arbitrary = identityGen
+
+identityGenInt :: Gen (Identity Int)
+identityGenInt = identityGen
+
+data Pair a b
+  = Pair a b
+  deriving (Eq, Show)
+
+pairGen :: (Arbitrary a, Arbitrary b) => Gen (Pair a b)
+pairGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Pair a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Pair a b) where
+  arbitrary = pairGen
+
+pairGenIntString :: Gen(Pair Int String)
+pairGenIntString = pairGen
 
 prop_additionGreater :: Int -> Bool
 prop_additionGreater x = x + 1 > x
