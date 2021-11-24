780639d778940d76e0237f7de11f34326071c6f0
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-23 22:03:53 -0500

    mad libbin

--- ch15/exercises.hs
+++ ch15/exercises.hs
@@ -20,3 +20,24 @@ instance Monoid a => Semigroup (Optional a) where
 
 instance Monoid a => Monoid (Optional a) where
   mempty = Nada
+
+type Adjective = String
+
+type Adverb = String
+
+type Noun = String
+
+type Exclamation = String
+
+madlibbinBetter :: Exclamation -> Adverb -> Noun -> Adjective -> String
+madlibbinBetter e adv noun adj =
+  mconcat
+    [ e,
+      "! he said ",
+      adv,
+      " as he jumped into his car ",
+      noun,
+      " and drove off with his ",
+      adj,
+      " wife"
+    ]
