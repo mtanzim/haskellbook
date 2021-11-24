77c93f79d0f2ccd5bc686577eb3a13037a3e0b0d
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-11-24 10:15:21 -0500

    set up monoid exercises

--- ch15/exercises.hs
+++ ch15/exercises.hs
@@ -1,5 +1,7 @@
 module Exercises where
 
+import Test.QuickCheck
+
 data Booly a = False' | True' deriving (Eq, Show)
 
 instance Semigroup (Booly a) where
@@ -41,3 +43,34 @@ madlibbinBetter e adv noun adj =
       adj,
       " wife"
     ]
+
+newtype First' a = First' {getFirst :: Optional a}
+  deriving (Eq, Show)
+
+instance Semigroup (First' a) where
+  (<>) = undefined
+
+instance Monoid (First' a) where
+  mempty = undefined
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend = First' String -> First' String -> First' String -> Bool
+
+type FstId = First' String -> Bool
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity a = (mempty <> a) == a
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity a = (a <> mempty) == a
+
+main :: IO ()
+main = do
+  quickCheck (monoidAssoc :: FirstMappend)
+  quickCheck (monoidLeftIdentity :: FstId)
+  quickCheck (monoidRightIdentity :: FstId)
