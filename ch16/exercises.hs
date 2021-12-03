3d2ec29bf8c0972b2faefbd8bdfdf64553622b67
Tanzim Mokammel <mtanzim@gmail.com>
2021-12-02 21:20:58 -0500

    do you lift bro?

--- ch16/exercises.hs
+++ ch16/exercises.hs
@@ -1,3 +1,20 @@
 module Exercises where
 
+a :: [Int]
+a = fmap (+1) (read "[1]" :: [Int])
 
+b :: Maybe [String]
+b = (fmap.fmap) (++ "lol") (Just ["Hi,", "Hello"])
+
+c :: Integer -> Integer
+c = fmap (*2) (\x -> x - 2)
+
+d :: Integer -> [Char]
+d = fmap ((return '1' ++) . show) (\x -> [x, 1..3])
+
+e :: IO Integer 
+e = let 
+        ioi = readIO "1" :: IO Integer 
+        changed = fmap read (fmap ("123"++) (fmap show ioi))
+        in 
+            fmap (*3) changed
