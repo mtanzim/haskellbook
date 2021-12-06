50b2b9dab835b700d6a860c79aa3177176f08a1a
Tanzim Mokammel <mtanzim@gmail.com>
2021-12-06 15:00:43 -0500

    names

--- advent-of-code-2021/day5.hs
+++ advent-of-code-2021/day5.hs
@@ -56,12 +56,12 @@ day5Input :: IO [LineDefinition]
 day5Input = do
   inputs <- readFile "day5Input.txt"
 
-  let parserA = ((map (splitOn " -> ")) . lines)
-      parserB = map (map (splitOn ","))
-      parserC = map (map (map (\x -> read x :: Integer)))
-      parserD = map (map (\lines -> (lines !! 0, lines !! 1)))
-      parserE = map (\line -> (line !! 0, line !! 1))
-   in return ((parserE . parserD . parserC . parserB . parserA) inputs)
+  let parseLines = ((map (splitOn " -> ")) . lines)
+      parsePoints = map (map (splitOn ","))
+      convertPointToInteger = map (map (map (\x -> read x :: Integer)))
+      arrToTupleOuter = map (map (\lines -> (lines !! 0, lines !! 1)))
+      arrToTupleInner = map (\line -> (line !! 0, line !! 1))
+   in return ((arrToTupleInner . arrToTupleOuter . convertPointToInteger . parsePoints . parseLines) inputs)
 
 main :: IO ()
 main = do
