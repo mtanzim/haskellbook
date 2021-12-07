2143be66cf2672bdbf129749982c3a55184dbbc4
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-07 12:15:02 -0500

    day 7

--- advent-of-code-2021/day7.hs
+++ advent-of-code-2021/day7.hs
@@ -12,19 +12,30 @@ main :: IO ()
 main = do
   input <- day7Input
   let sortedInput = sort input
-   in print (gatherRelativeDistances sortedInput 0 (0, sum sortedInput) sum)
+      range = [head sortedInput .. last sortedInput]
+      maxPossibleConst = sum range
+      maxPossibleWithAccel = sumWithAcceleration range
+   in do
+        -- TODO: fix slow algos!! part b is O(n^3)!!
+        print (gatherRelativeDistances sortedInput range 0 (0, maxPossibleConst) sum)
+        print (gatherRelativeDistances sortedInput range 0 (0, maxPossibleWithAccel) sumWithAcceleration)
 
-gatherRelativeDistances :: [Integer] -> Int -> (Integer, Integer) -> ([Integer] -> Integer) -> (Integer, Integer)
-gatherRelativeDistances lst curIdx (minPos, minTotalDistance) summingFn =
-  if curIdx == length lst - 1
+sumWithAcceleration :: [Integer] -> Integer
+sumWithAcceleration = sum . map (\x -> sum [0 .. x])
+
+gatherRelativeDistances :: [Integer] -> [Integer] -> Int -> (Integer, Integer) -> ([Integer] -> Integer) -> (Integer, Integer)
+gatherRelativeDistances lst range curIdx (minPos, minTotalDistance) summingFn =
+  if curIdx == length range - 1
     then (minPos, minTotalDistance)
     else
-      let curVal = lst !! curIdx
+      let curVal = range !! curIdx
           relativeDistances = map (\x -> abs (curVal - x)) lst
           totalDisplacement = summingFn relativeDistances
           latestMinDistance = min totalDisplacement minTotalDistance
           latestMinPos = if latestMinDistance < minTotalDistance then curVal else minPos
-       in gatherRelativeDistances lst (curIdx + 1) (latestMinPos, latestMinDistance) summingFn
+       in gatherRelativeDistances lst range (curIdx + 1) (latestMinPos, latestMinDistance) summingFn
+
+-- DEBUG
 
 testInput :: [Integer]
 testInput = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
@@ -32,4 +43,5 @@ testInput = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 testMain :: (Integer, Integer)
 testMain =
   let sortedInput = sort testInput
-   in gatherRelativeDistances sortedInput 0 (0, sum sortedInput) sum
+      range = [head sortedInput .. last sortedInput]
+   in gatherRelativeDistances sortedInput range 0 (0, sumWithAcceleration range) sumWithAcceleration
