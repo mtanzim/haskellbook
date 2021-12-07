ef22f395f7a3e24e7f63ca0910b7613cd47f57ef
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-07 18:49:37 -0500

    start hashmap

--- advent-of-code-2021/day6.hs
+++ advent-of-code-2021/day6.hs
@@ -1,5 +1,18 @@
 module Day6 where
 
+import Data.List.Split
+import qualified Data.Map as Map
+
+day6Input :: IO [Integer]
+day6Input = do
+  inputs <- readFile "day6Input.txt"
+  return (map (\x -> read x :: Integer) (splitOn "," inputs))
+
+main :: IO ()
+main = do
+  input <- day6Input
+  print (fishSimulator input 80)
+
 testInput :: [Integer]
 testInput = [3, 4, 3, 1, 2]
 
@@ -15,5 +28,10 @@ countZeroes = foldr (\x acc -> if x == 0 then acc + 1 else acc) 0
 fishSimulator :: [Integer] -> Integer -> Int
 fishSimulator initState days = length (foldr (\day acc -> elapse acc) initState [1 .. days])
 
+buildStatusMap :: [Integer] -> Map.Map Integer Integer
+buildStatusMap = foldr (\x curMap -> Map.insert x (newValue curMap x) curMap) Map.empty
+  where
+    newValue curMap x = (Map.findWithDefault 0 x curMap) + 1
+
 testMain :: Int
-testMain = fishSimulator testInput 80
+testMain = fishSimulator testInput 256
