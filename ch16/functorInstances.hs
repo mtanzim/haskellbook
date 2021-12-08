eb1894611f0ea591a2d68896f2d16ef91c4b9070
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-08 18:02:35 -0500

    two functor

--- ch16/functorInstances.hs
+++ ch16/functorInstances.hs
@@ -36,17 +36,37 @@ data Pair a = Pair a a deriving (Eq, Show)
 instance Functor Pair where
   fmap f (Pair a a') = Pair (f a) (f a')
 
+data Two a b = Two a b deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+twoGen :: (Arbitrary a, Arbitrary b) => Gen (Two a b)
+twoGen = do
+  a <- arbitrary
+  b <- arbitrary
+  return (Two a b)
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = twoGen
+
 main :: IO ()
 main = do
   quickCheck f
   quickCheck li
   quickCheck fPair
+  quickCheck liPair
+  quickCheck fTwo
   where
     f :: Identity Int -> Bool
-    fPair :: Pair Int -> Bool
     f x = functorIdentity x
+    fPair :: Pair Int -> Bool
     fPair x = functorIdentity x
+    fTwo :: Two Int Char -> Bool
+    fTwo x = functorIdentity x
     c = functorCompose (+ 1) (* 2)
     li x = c (x :: Identity Int)
     cPair = functorCompose (+ 1) (* 2)
     liPair x = cPair (x :: Pair Int)
+    cTwo = functorCompose (const Two 44 "s") (const "s")
+    liTwo x = cTwo (x :: Two Int String)
