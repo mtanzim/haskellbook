bf5bc604cfc4fc49782f9d639a153545d3f0b5b4
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-11 17:36:14 -0500

    more functor exercises

--- ch16/chapterExercises.hs
+++ ch16/chapterExercises.hs
@@ -31,3 +31,32 @@ newtype Flip f a b = Flip (f b a) deriving (Eq, Show)
 instance Functor (Flip More a) where
   fmap f (Flip (L a b a')) = Flip $ L (f a) b (f a')
   fmap f (Flip (R b a b')) = Flip $ R b (f a) b'
+
+data Quant a b = Finance | Desk a | Bloor b
+
+instance Functor (Quant a) where
+  fmap _ Finance = Finance
+  fmap _ (Desk a) = Desk a
+  fmap f (Bloor b) = Bloor (f b)
+
+-- data K a b = K a
+
+instance Functor (K a) where
+  fmap _ (K a) = K a
+
+newtype K a b = K a
+
+instance Functor (Flip K a) where
+  fmap f (Flip (K a)) = Flip $ K (f a)
+
+data EvilGoateeConst a b = GoatyConst b deriving (Eq, Show)
+
+instance Functor (EvilGoateeConst a) where
+  fmap f (GoatyConst b) = GoatyConst (f b)
+
+data LiftItOut f a = LiftItOut (f a) deriving (Eq, Show)
+
+instance Functor f => Functor (LiftItOut f) where
+  fmap f (LiftItOut ga) = LiftItOut (fmap f ga)
+
+data Parappa f g a = Dawrappa (f a) (g a) deriving (Eq, Show)
