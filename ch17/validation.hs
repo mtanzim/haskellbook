0656c46b5deeac16b7233da52daa910c95f81cae
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-15 21:12:45 -0500

    validation examples

--- ch17/validation.hs
+++ ch17/validation.hs
@@ -1,5 +1,7 @@
 module Validation where
 
+import Control.Applicative
+
 validateLength :: Int -> String -> Maybe String
 validateLength maxLen s =
   if length s > maxLen
@@ -11,7 +13,7 @@ newtype Name = Name String deriving (Eq, Show)
 newtype Address = Address String deriving (Eq, Show)
 
 mkName :: String -> Maybe Name
-mkName s = fmap Name (validateLength 15 s)
+mkName s = fmap Name (validateLength 3 s)
 
 mkAddress :: String -> Maybe Address
 mkAddress a = fmap Address (validateLength 100 a)
@@ -30,6 +32,29 @@ mkPerson' :: String -> String -> Maybe Person
 mkPerson' n a =
   --   (fmap Person (mkName n)) <*> mkAddress a
   Person <$> mkName n <*> mkAddress a
+
+data Cow = Cow
+  { name :: String,
+    age :: Int,
+    weight :: Int
+  }
+
+noEmpty :: [Char] -> Maybe [Char]
+noEmpty "" = Nothing
+noEmpty s = Just s
+
+noNegative :: Int -> Maybe Int
+noNegative n
+  | n >= 0 = Just n
+  | otherwise = Nothing
+
+cowFromString :: String -> Int -> Int -> Maybe Cow
+cowFromString name age weight =
+  Cow <$> noEmpty name <*> noNegative age <*> noNegative weight
+
+cowFromString' :: String -> Int -> Int -> Maybe Cow
+cowFromString' name age weight =
+  liftA3 Cow (noEmpty name) (noNegative age) (noNegative weight)
 
 main :: Maybe Person
 main =
