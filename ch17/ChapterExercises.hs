eca6f83710ac4808465d9c27465008bacbcb30c9
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-22 09:14:20 -0700

    quick batch applicative for pair

--- ch17/ChapterExercises.hs
+++ ch17/ChapterExercises.hs
@@ -1,5 +1,10 @@
 module ChapterExercises where
 
+import Test.QuickCheck
+import Test.QuickCheck.Arbitrary
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 data Pair a = Pair a a deriving (Eq, Show)
 
 -- instance Semigroup a => Semigroup (Pair a) where
@@ -15,3 +20,18 @@ instance Functor Pair where
 instance Applicative Pair where
   pure a = Pair a a
   Pair f f' <*> Pair x x' = Pair (f x) (f' x')
+
+pairGen :: Arbitrary a => Gen (Pair a)
+pairGen = do
+  a <- arbitrary
+  return (Pair a a)
+
+instance Arbitrary a => Arbitrary (Pair a) where
+  arbitrary = pairGen
+
+instance Eq a => EqProp (Pair a) where
+  (=-=) = eq
+
+main :: IO ()
+main = do
+    quickBatch $ applicative (Pair ('a', 'b', 'c') ('d', 'e', 'f'))
