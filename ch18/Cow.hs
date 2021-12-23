91fe09ff6981326c716e29d919b70d95d3d954d9
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-22 17:30:39 -0700

    monadic cow

---
+++ ch18/Cow.hs
@@ -0,0 +1,47 @@
+module Cow where
+
+data Cow = Cow
+  { name :: String,
+    age :: Int,
+    weight :: Int
+  }
+  deriving (Eq, Show)
+
+noEmpty :: [Char] -> Maybe [Char]
+noEmpty "" = Nothing
+noEmpty s = Just s
+
+noNegative :: Int -> Maybe Int
+noNegative n
+  | n >= 0 = Just n
+  | otherwise = Nothing
+
+weightCheck :: Cow -> Maybe Cow
+weightCheck c =
+  let w = weight c
+      n = name c
+   in if n == "Bess" && w > 499
+        then Nothing
+        else Just c
+
+makeSphericalCow :: String -> Int -> Int -> Maybe Cow
+makeSphericalCow name' age' weight' =
+  case noEmpty name' of
+    Nothing -> Nothing
+    Just nammy -> case noNegative age' of
+      Nothing -> Nothing
+      Just agey ->
+        case noNegative weight' of
+          Nothing -> Nothing
+          Just weighty ->
+            weightCheck (Cow nammy agey weighty)
+
+-- sugar with do
+-- note that this cannot be done with applicative b/c weightCheck expects a Cow and NOT a Maybe Cow
+-- See for comparison: 
+makeSphericalCow' :: String -> Int -> Int -> Maybe Cow
+makeSphericalCow' name' age' weight' = do
+  nammy <- noEmpty name'
+  agey <- noNegative age'
+  weighty <- noNegative weight'
+  weightCheck (Cow nammy agey weighty)
