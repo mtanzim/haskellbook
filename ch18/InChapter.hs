aa192d79eb2e45391d6acccfded52fb4594b26a0
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-31 17:48:04 -0700

    monda instance for either

--- ch18/InChapter.hs
+++ ch18/InChapter.hs
@@ -16,3 +16,21 @@ crossProduct xs ys = do
   x <- xs
   y <- ys
   [x, y]
+
+data Sum a b = First a | Second b deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second (f b)
+
+instance Monoid a => Applicative (Sum a) where
+  pure b = Second b
+  First a <*> First a' = First (a <> a')
+  First a <*> _ = First a
+  _ <*> First a = First a
+  Second f <*> Second a = Second (f a)
+
+instance Monoid a => Monad (Sum a) where
+  return = pure
+  (First a) >>= _ = First a
+  (Second b) >>= f = f b
