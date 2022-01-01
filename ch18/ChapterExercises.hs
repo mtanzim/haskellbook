20fd424752a7e0723b8df8684863dd1bf7a61381
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-31 18:49:11 -0700

    nope monad

--- ch18/ChapterExercises.hs
+++ ch18/ChapterExercises.hs
@@ -5,6 +5,25 @@ import Test.QuickCheck.Arbitrary
 import Test.QuickCheck.Checkers
 import Test.QuickCheck.Classes
 
+data Nope a = NopeDotJpg deriving (Eq, Show)
+
+instance Functor Nope where
+  fmap _ NopeDotJpg = NopeDotJpg
+
+instance Applicative Nope where
+  pure _ = NopeDotJpg
+  _ <*> _ = NopeDotJpg
+
+instance Monad Nope where
+  return = pure
+  _ >>= _ = NopeDotJpg
+
+instance Arbitrary (Nope a) where
+  arbitrary = elements [NopeDotJpg]
+
+instance (Eq a) => EqProp (Nope a) where
+  (=-=) = eq
+
 data BahEither b a = PRight b | PLeft a deriving (Eq, Show)
 
 instance Functor (BahEither b) where
@@ -18,7 +37,7 @@ instance Monoid b => Applicative (BahEither b) where
   _ <*> PRight b = PRight b
   PLeft f <*> PLeft a = PLeft (f a)
 
-instance Monoid a => Monad (BahEither a) where
+instance Monoid b => Monad (BahEither b) where
   return = pure
   (PRight b) >>= _ = PRight b
   (PLeft a) >>= f = f a
@@ -40,5 +59,7 @@ test trigger = do
   quickBatch $ applicative trigger
   quickBatch $ monad trigger
 
+main :: IO ()
 main = do
   test (undefined :: BahEither String (Int, Int, Int))
+  test (undefined :: Nope (String, String, String))
