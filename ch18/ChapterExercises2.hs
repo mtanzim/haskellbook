ea5e8e085e8ab26b0745904f84a570fc94867150
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2021-12-31 19:47:43 -0700

    .

---
+++ ch18/ChapterExercises2.hs
@@ -0,0 +1,15 @@
+module ChapterExercises2 where
+
+import Control.Monad (ap, join, liftM2)
+
+j :: Monad m => m (m a) -> m a
+j = join
+
+l1 :: Monad m => (a -> b) -> m a -> m b
+l1 = fmap
+
+l2 :: Monad m => (a -> b -> c) -> m a -> m b -> m c
+l2 = liftM2
+
+a :: Monad m => m a -> m (a -> b) -> m b
+a = flip ap
