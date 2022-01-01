a7d2100b3c99c6a65d1cb4bf066c798c3f0a104f
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-01 13:17:48 -0700

    try to understand final two questions of monad chapter

--- ch18/ChapterExercises2.hs
+++ ch18/ChapterExercises2.hs
@@ -14,5 +14,22 @@ l2 = liftM2
 a :: Monad m => m a -> m (a -> b) -> m b
 a = flip ap
 
+-- TODO: come back to this
+-- solution: https://github.com/andrewMacmurray/haskell-book-solutions/blob/f4fd386187c03828d1736d9a43642ab4f0ec6462/src/ch18/Exercises2.hs#L20
 meh :: Monad m => [a] -> (a -> m b) -> m [b]
 meh [] _ = return []
+meh (cur : rest) f = liftM2 (++) (fmap (: []) (f cur)) (meh rest f)
+
+-- come back to this
+flipType :: (Monad m) => [m a] -> m [a]
+flipType lst = meh lst id
+
+-- Some hints
+-- *ChapterExercises2 Control.Monad Data.Monoid> :t liftM2
+-- liftM2 :: Monad m => (a1 -> a2 -> r) -> m a1 -> m a2 -> m r
+-- *ChapterExercises2 Control.Monad Data.Monoid> :t (++)
+-- (++) :: [a] -> [a] -> [a]
+-- *ChapterExercises2 Control.Monad Data.Monoid> :t (:[])
+-- (:[]) :: a -> [a]
+-- *ChapterExercises2 Control.Monad Data.Monoid> fmap (:[]) [1,2,3]
+-- [[1],[2],[3]]
