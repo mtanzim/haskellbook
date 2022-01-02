df34af1e5b1214a756a16bf987c7e7a260f51efb
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-01 18:29:29 -0700

    failed attempt at minimum

--- ch20/LibFunctions.hs
+++ ch20/LibFunctions.hs
@@ -4,4 +4,20 @@ import Data.Foldable
 import Data.Monoid
 
 sum' :: (Foldable t, Num a) => t a -> a
-sum' t = getSum $ foldMap Sum t
+sum' = getSum . foldMap Sum
+
+product' :: (Foldable t, Num a) => t a -> a
+product' = getProduct . foldMap Product
+
+elem' :: (Foldable t, Eq a) => a -> t a -> Bool
+elem' a t = getAny $ foldMap (Any . (== a)) t
+
+-- TODO: agh come back to this
+-- minimum' :: (Foldable t, Ord a) => t a -> Maybe a
+-- minimum' = foldr fn Nothing
+--   where
+--     fn acc cur = case (acc, cur) of
+--       (Nothing, cur') -> Just cur'
+--       (Just acc', cur') -> if cur' < acc' then Just cur' else Just acc'
+
+-- maximum' = (Foldable t, Ord a) => t a -> Maybe a
