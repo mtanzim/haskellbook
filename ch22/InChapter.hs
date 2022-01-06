c4de71c5233e6a201cabc2a559ac380f7388674a
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-06 16:36:57 -0500

    applicative for Reader

--- ch22/InChapter.hs
+++ ch22/InChapter.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE InstanceSigs #-}
+
 module InChapter where
 
 newtype Reader r a = Reader {runReader :: r -> a}
@@ -12,3 +13,15 @@ asks = Reader
 myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 myLiftA2 f a b = f <$> a <*> b
 
+instance Functor (Reader r) where
+  fmap f (Reader ra) = Reader $ \r -> f (ra r)
+
+instance Applicative (Reader r) where
+  pure :: a -> Reader r a
+  pure a = Reader (\_ -> a)
+  (<*>) ::
+    Reader r (a -> b) ->
+    Reader r a ->
+    Reader r b
+  (Reader rab) <*> (Reader ra) =
+    Reader $ \r -> rab r (ra r)
