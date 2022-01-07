0c08a257c3e9df638d3ebdb7438e93d5a09adcc9
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-07 17:20:11 -0500

    setup reader practice

---
+++ ch22/ReaderPractice.hs
@@ -0,0 +1,39 @@
+module ReaderPractice where
+
+-- import Data.List (find)
+
+x = [1, 2, 3]
+
+y = [4, 5, 6]
+
+z = [7, 8, 9]
+
+-- lookup' :: Eq a => a -> [(a, b)] -> Maybe b
+-- lookup' a lst = snd <$> find (\(a', _) -> a' == a) lst
+
+xs :: Maybe Integer
+xs = lookup 3 (zip x y)
+
+ys :: Maybe Integer
+ys = lookup 6 (zip y z)
+
+zs :: Maybe Integer
+zs = lookup 4 (zip x y)
+
+z' :: Integer -> Maybe Integer
+z' n = lookup n (zip x z)
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 n = (,) (z' n) (z' n)
+
+summed :: Num c => (c, c) -> c
+summed = uncurry (+)
+
+bolt :: Integer -> Bool
+bolt = (&&) <$> (> 3) <*> (< 8)
