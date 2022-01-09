77dffba5632f0c9810ee6d6fed88c2e91b065159
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-08 20:46:36 -0500

    functor and applicative for State

---
+++ ch23/Moi.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE InstanceSigs #-}
+
+module Moi where
+
+newtype Moi s a = Moi {runMoi :: s -> (a, s)}
+
+instance Functor (Moi s) where
+  fmap :: (a -> b) -> Moi s a -> Moi s b
+  fmap f (Moi g) = Moi $ \s -> (f (fst (g s)), s)
+
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = Moi $ \s -> (a, s)
+  (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+  (Moi f) <*> (Moi g) = Moi $ \s -> (fst (f s) (fst (g s)), s)
