bbafe3bad4375e905f8bb6b1965b5763544d7441
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-10 16:18:07 -0500

    start parsers

---
+++ ch24/LearnParsers.hs
@@ -0,0 +1,54 @@
+module LearnParsers where
+
+import DriverPipeline (oneShot)
+import Text.Trifecta
+
+stop :: Parser a
+stop = unexpected "stop"
+
+one :: Parser Char
+one = char '1'
+
+oneDone :: Parser ()
+oneDone = one >> eof
+
+one' :: Parser b
+one' = one >> stop
+
+oneTwo :: Parser Char
+oneTwo = char '1' >> char '2'
+
+oneTwo' :: Parser b
+oneTwo' = oneTwo >> stop
+
+oneTwoThreeDone :: Parser ()
+oneTwoThreeDone = oneTwo >> char '3' >> eof
+
+testParse :: Parser Char -> IO ()
+testParse p = print $ parseString p mempty "123"
+
+pNL :: [Char] -> IO ()
+pNL s = putStrLn ('\n' : s)
+
+main = do
+  pNL "stop:"
+  testParse stop
+  pNL "one:"
+  testParse one
+  pNL "one':"
+  testParse one'
+  pNL "oneTwo:"
+  testParse oneTwo
+  pNL "oneTwo':"
+  testParse oneTwo'
+  pNL "oneDone"
+  print $ parseString oneDone mempty "123"
+  pNL "oneDone success"
+  print $ parseString oneDone mempty "1"
+  pNL "oneTwoThreeDone success"
+  print $ parseString oneTwoThreeDone mempty "123"
+  pNL "oneTwoThreeDone failure"
+  print $ parseString oneTwoThreeDone mempty "1234"
+  pNL "oneTwoThreeDone failure 2"
+  print $ parseString oneTwoThreeDone mempty "12"
+
