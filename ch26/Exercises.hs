25c0e062ac24cf894aa92d98618db4243b5d80d7
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-21 18:30:18 -0500

    MOAR EXCITE

--- ch26/Exercises.hs
+++ ch26/Exercises.hs
@@ -1,9 +1,27 @@
 module Exercises where
 
 import Control.Monad.Trans.Reader
+import Control.Monad.Trans.State
+import Data.Functor.Identity
 
 rDec :: Num a => Reader a a
 rDec = ReaderT $ \r -> return (r - 1)
 
 rDec' :: Num a => Reader a a
 rDec' = ReaderT $ return . (+ (-1))
+
+rShow :: Show a => ReaderT a Identity String
+rShow = ReaderT $ \a -> return (show a)
+
+rShow' :: Show a => ReaderT a Identity String
+rShow' = ReaderT $ return . show
+
+rPrintAndInc :: (Num a, Show a) => ReaderT a IO a
+rPrintAndInc = ReaderT $ \a -> do
+  putStrLn ("Hi: " ++ show a)
+  return (a + 1)
+
+sPrintIncAccum :: (Num a, Show a) => StateT a IO String
+sPrintIncAccum = StateT $ \a -> do
+  putStrLn ("Hi: " ++ show a)
+  return (show a, a + 1)
