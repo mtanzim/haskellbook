eb7c83655e85bc77284ec2a5f22247ac69f3975d
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-23 13:34:23 -0500

    basic game

--- ch26/Morra.hs
+++ ch26/Morra.hs
@@ -1,11 +1,10 @@
 module Morra where
 
+import Data.Bool (Bool)
 import Data.Maybe (fromMaybe)
+import System.Random
 import Text.Read (readMaybe)
-
--- data Odds = Odd
-
--- data Evens = Even
+import Control.Monad (forever)
 
 data PlayerState = Odd | Even deriving (Eq, Show)
 
@@ -27,6 +26,13 @@ getFingers s =
         Nothing -> Nothing
         Just n -> if n >= 0 && n <= 5 then Just n else Nothing
 
+didPlayerWin :: Config -> Int -> Bool
+didPlayerWin config sum =
+  case (player1 config, sum `mod` 2) of
+    (Even, 0) -> True
+    (Odd, 1) -> True
+    _ -> False
+
 main :: IO ()
 main = do
   putStrLn "Pick odd or even: 0 for even, 1 for Odd"
@@ -34,10 +40,18 @@ main = do
   let config = getConfig ans
   case config of
     Nothing -> error "Invalid input"
-    Just con -> print config
-  putStrLn "How many fingers? (0 to 5)"
-  fingers <- getLine
-  let fingersN = getFingers fingers
-  case fingersN of 
-    Nothing -> error "Invalid input"
-    Just n -> putStrLn fingers
+    Just con -> forever $ do
+      print config
+      putStrLn "How many fingers? (0 to 5)"
+      fingers <- getLine
+      cpuVal <- randomRIO (0 :: Int, 5 :: Int)
+      let fingersN = getFingers fingers
+      case fingersN of
+        Nothing -> putStrLn "Invalid input, try again"
+        Just n -> do
+          let sum = n + cpuVal
+          putStrLn $ "CPU picked: " ++ show cpuVal
+          putStrLn $ "Sum: " ++ show sum
+          putStr "Did you win? "
+          print $ didPlayerWin con sum
+          putStrLn "\n"
