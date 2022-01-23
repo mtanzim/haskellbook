97127bee2d38c4ea85403d6a53b25906bcc3c7de
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-23 14:43:02 -0500

    works, but I need more time to understand

--- ch26/Morra.hs
+++ ch26/Morra.hs
@@ -1,15 +1,18 @@
 module Morra where
 
+import Control.Monad (Monad (return), forever)
+import Control.Monad.State
 import Data.Bool (Bool)
+import Data.Int (Int)
+import qualified Data.Map as M
 import Data.Maybe (fromMaybe)
 import System.Random
 import Text.Read (readMaybe)
-import Control.Monad (forever)
 
 data PlayerState = Odd | Even deriving (Eq, Show)
 
 data Config = Config
-  { player1 :: PlayerState
+  { playerState :: PlayerState
   }
   deriving (Eq, Show)
 
@@ -28,11 +31,20 @@ getFingers s =
 
 didPlayerWin :: Config -> Int -> Bool
 didPlayerWin config sum =
-  case (player1 config, sum `mod` 2) of
+  case (playerState config, sum `mod` 2) of
     (Even, 0) -> True
     (Odd, 1) -> True
     _ -> False
 
+updateScore :: Bool -> StateT Int IO Int
+updateScore won = do
+  cv <- get
+  if won
+    then put $ cv + 1
+    else put cv
+  get
+
+-- https://wiki.haskell.org/Simple_StateT_use
 main :: IO ()
 main = do
   putStrLn "Pick odd or even: 0 for even, 1 for Odd"
@@ -40,18 +52,26 @@ main = do
   let config = getConfig ans
   case config of
     Nothing -> error "Invalid input"
-    Just con -> forever $ do
-      print config
-      putStrLn "How many fingers? (0 to 5)"
-      fingers <- getLine
-      cpuVal <- randomRIO (0 :: Int, 5 :: Int)
-      let fingersN = getFingers fingers
-      case fingersN of
-        Nothing -> putStrLn "Invalid input, try again"
-        Just n -> do
-          let sum = n + cpuVal
-          putStrLn $ "CPU picked: " ++ show cpuVal
-          putStrLn $ "Sum: " ++ show sum
-          putStr "Did you win? "
-          print $ didPlayerWin con sum
-          putStrLn "\n"
+    Just con ->
+      runStateT
+        ( forever $ do
+            liftIO $ print con
+            liftIO $ putStrLn "How many fingers? (0 to 5)"
+            fingers <- liftIO getLine
+            cpuVal <- randomRIO (0 :: Int, 5 :: Int)
+            let fingersN = getFingers fingers
+            case fingersN of
+              Nothing -> liftIO $ putStrLn "Invalid input, try again.\n\n"
+              Just n -> do
+                let sum = n + cpuVal
+                    playerWon = didPlayerWin con sum
+                liftIO $ putStrLn $ "CPU picked: " ++ show cpuVal
+                liftIO $ putStrLn $ "Sum: " ++ show sum
+                liftIO $ putStrLn $ "Did you win? " ++ show playerWon
+                gv <- updateScore playerWon
+                liftIO $ putStr $ "Player score: "
+                liftIO $ print gv
+                liftIO $ putStrLn ""
+        )
+        0
+        >> return ()
