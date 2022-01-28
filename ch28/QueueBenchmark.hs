5325d3186a44c250ccd3b5a142da0f5a1a9bdca2
Tanzim Mokammel <tanzim.mokammel@flipp.com>
2022-01-28 18:24:12 -0500

    compare with sequence

--- ch28/QueueBenchmark.hs
+++ ch28/QueueBenchmark.hs
@@ -1,6 +1,7 @@
 module QueueBenchmark where
 
 import Criterion.Main
+import qualified Data.Sequence as S
 
 data Queue a = Queue
   { enqueue :: [a],
@@ -69,13 +70,38 @@ popLockDrop n =
                 Nothing -> q
         else go (n - 1) (push lastV q) lastV
 
+seqPop :: S.Seq a -> Maybe (a, S.Seq a)
+seqPop s = case (S.lookup 0 s) of
+  Just v -> Just (v, S.deleteAt 0 s)
+  Nothing -> Nothing
+
+seqPush :: a -> S.Seq a -> S.Seq a
+seqPush a s = S.insertAt (S.length s) a s
+
+-- TODO: polymorphic function to cater all 3 dance functions?
+lineDance :: Int -> S.Seq Int
+lineDance n =
+  let z = S.fromList zLst
+   in go n z 33
+  where
+    go 0 q _ = q
+    go n q lastV =
+      if even n
+        then
+          let val = seqPop q
+           in case val of
+                Just (v, q') -> go (n - 1) q' v
+                Nothing -> q
+        else go (n - 1) (seqPush lastV q) lastV
+
 zLst :: [Int]
-zLst = [1 .. 1000]
+zLst = [1 .. 100]
 
 main :: IO ()
 main =
   defaultMain
     [ bench "two stack Q" $ whnf popLockDrop 123456,
+      bench "sequence" $ whnf lineDance 123456,
       bench "one stack Q" $ whnf boringDance 123456
     ]
 
@@ -123,3 +149,25 @@ main =
 -- mean                 4.495 s    (4.244 s .. 4.808 s)
 -- std dev              323.7 ms   (130.0 ms .. 445.6 ms)
 -- variance introduced by outliers: 20% (moderately inflated)
+
+-- with Sequence, List size = 100
+-- benchmarking two stack Q
+-- time                 138.9 ms   (133.8 ms .. 146.0 ms)
+--                      0.997 R²   (0.989 R² .. 1.000 R²)
+-- mean                 137.8 ms   (135.6 ms .. 140.5 ms)
+-- std dev              3.939 ms   (3.017 ms .. 5.383 ms)
+-- variance introduced by outliers: 11% (moderately inflated)
+
+-- benchmarking sequence
+-- time                 129.2 ms   (128.0 ms .. 130.7 ms)
+--                      1.000 R²   (1.000 R² .. 1.000 R²)
+-- mean                 130.3 ms   (129.3 ms .. 132.8 ms)
+-- std dev              2.320 ms   (574.1 μs .. 3.545 ms)
+-- variance introduced by outliers: 11% (moderately inflated)
+
+-- benchmarking one stack Q
+-- time                 670.3 ms   (514.9 ms .. 808.9 ms)
+--                      0.994 R²   (0.976 R² .. 1.000 R²)
+-- mean                 704.2 ms   (676.8 ms .. 727.7 ms)
+-- std dev              28.68 ms   (23.20 ms .. 32.74 ms)
+-- variance introduced by outliers: 19% (moderately inflated)
